974d578229c7c1282d73dee169547148adffac1b
Pedro Gomes <pedrogomespeixoto14@gmail.com>
2023-01-07 13:03:32 -0200

    classes do dominio criadas

---
+++ back-end/src/main/java/com/backend/backend/domain/Chamado.java
@@ -0,0 +1,121 @@
+package com.backend.backend.domain;
+
+import com.backend.backend.domain.enums.Prioridade;
+import com.backend.backend.domain.enums.Status;
+
+import java.time.LocalDate;
+
+public class Chamado {
+
+    private Integer id;
+    private LocalDate dateAbertura = LocalDate.now();
+    private LocalDate dataFechamento;
+    private Prioridade prioridade;
+    private Status status;
+    private String titulo;
+    private String observacoes;
+
+    private Tecnico tecnico;
+    private Cliente cliente;
+
+    public Chamado() {
+        super();
+    }
+
+    public Chamado(Integer id, Prioridade prioridade, Status status, String titulo, String observacoes, Tecnico tecnico, Cliente cliente) {
+        this.id = id;
+        this.prioridade = prioridade;
+        this.status = status;
+        this.titulo = titulo;
+        this.observacoes = observacoes;
+        this.tecnico = tecnico;
+        this.cliente = cliente;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getDateAbertura() {
+        return dateAbertura;
+    }
+
+    public void setDateAbertura(LocalDate dateAbertura) {
+        this.dateAbertura = dateAbertura;
+    }
+
+    public LocalDate getDataFechamento() {
+        return dataFechamento;
+    }
+
+    public void setDataFechamento(LocalDate dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
+
+    public Prioridade getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Prioridade prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public Tecnico getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(Tecnico tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Chamado chamado = (Chamado) o;
+
+        return getId() != null ? getId().equals(chamado.getId()) : chamado.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
