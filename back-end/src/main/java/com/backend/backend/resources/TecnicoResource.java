0a8cacdd377ef7046b19a825e66ee2a3973b65f4
Pedro Gomes <pedrogomespeixoto14@gmail.com>
2023-01-10 02:32:57 -0200

    Padrão DTO para a classe Tecnico

--- back-end/src/main/java/com/backend/backend/resources/TecnicoResource.java
+++ back-end/src/main/java/com/backend/backend/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package com.backend.backend.resources;
 
 import com.backend.backend.domain.Tecnico;
+import com.backend.backend.domain.dtos.TecnicoDTO;
 import com.backend.backend.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +18,8 @@ public class TecnicoResource {
     private TecnicoService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 }
